a54cd2537e9f8756d6421884521a729c4e262973
Forkk <contact@forkk.net>
2020-01-21 20:23:27 -0600

    Add robots.txt

--- mcarch/views/__init__.py
+++ mcarch/views/__init__.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Blueprint, render_template, url_for, redirect, abort, flash
+from flask import Blueprint, render_template, url_for, redirect, abort, flash, send_file
 from sqlalchemy.exc import IntegrityError
 
 from mcarch.app import db
@@ -10,6 +10,10 @@ root = Blueprint('root', __name__, template_folder="templates")
 @root.route("/")
 def home():
     return render_template("home.html")
+
+@root.route("/robots.txt")
+def robots():
+    return send_file("templates/robots.txt")
 
 @root.app_errorhandler(404)
 def err404(err):
